e5806c3061940f3b81b6025de6906c53dd69cde6
Felicity Tarnell <felicity@loreley.flyingparchment.org.uk>
2014-02-23 01:59:36 +0000

    Clean up 'view source' and add footer link.

--- waiflike/views.py
+++ waiflike/views.py
@@ -6,7 +6,8 @@ from waiflike.models import SitePage
 
 def source(request, slug):
     p = get_object_or_404(SitePage, slug = slug)
-    return HttpResponse(p.body, content_type='text/plain')
+    return render(request, 'waiflike/source.html',
+        dictionary = { 'page': p })
 
 class EditForm(forms.Form):
     body = forms.CharField(widget = forms.Textarea)
